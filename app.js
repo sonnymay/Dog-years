fda8847e1df00a5cb5e7d8e558883c6db3f614ea
Sonny May <thesan_555@hotmail.com>
2022-09-19 23:13:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//my current age
+const myAge = 31;
+//early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Sonny".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
